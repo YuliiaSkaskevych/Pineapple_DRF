d3faad8faff064d4d5183b2b0f7f1fefe0a6f05d
yuliia00 <skaskevich2000@gmail.com>
2022-09-06 12:55:56 +0200

    add Token

--- quickstart/views.py
+++ quickstart/views.py
@@ -4,10 +4,6 @@ from .permissions import IsOwnerOrReadOnly
 from rest_framework import viewsets
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -28,9 +24,3 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
