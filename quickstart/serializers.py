6ca121535493d4eab32f562c95ac7960fb736909
yuliia00 <skaskevich2000@gmail.com>
2022-09-07 18:49:00 +0200

    final version

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -2,13 +2,17 @@ from .models import Post, Comment
 from rest_framework import serializers
 
 
-class PostSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
     class Meta:
         model = Post
-        fields = ['id', 'author', 'heading', 'text', 'create']
+        fields = ['id', 'author', 'heading', 'text', 'create', 'comments']
 
 
-class CommentSerializer(serializers.ModelSerializer):
+class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
     class Meta:
         model = Comment
         fields = ['id', 'author', 'text', 'post', 'publish']
